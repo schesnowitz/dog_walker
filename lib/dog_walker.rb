5e9a26198c9f879723eb22983436dc3127f4f83c
Stephen Chesnowitz <steve@chesnowitz.com>
2017-10-29 12:28:46 -0400

    first commit

---
+++ lib/dog_walker.rb
@@ -0,0 +1,11 @@
+require "dog_walker/version"
+
+module DogWalker
+  class Dog 
+
+    def walk_the_dog
+      puts "I am busy walking the dog" 
+    end
+
+  end
+end
---
+++ lib/dog_walker/version.rb
@@ -0,0 +1,3 @@
+module DogWalker
+  VERSION = "0.1.0"
+end
